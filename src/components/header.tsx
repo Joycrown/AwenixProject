83b0f441f6c8b2c97120d6df49854df675db07bb
hashkitti001 <iJeddy5.0@gmail.com>
2024-04-24 22:17:06 +0100

    add first version of moblie navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,42 @@
 import { Link } from "react-router-dom";
 import { logo } from "../assets";
-import { FaBars } from "react-icons/fa6";
+
+import { IoClose, IoMenu } from "react-icons/io5";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex">
       <header className="sm:px-16 px-8 py-4 z-10 bg-white w-full">
-
         <nav className="flex justify-between max-container items-center">
-          <button className="sm:block md:block lg:hidden">
-            <FaBars fontSize={'2rem'} />
+          <button
+            className="sm:block md:block lg:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+           { isOpen ? <IoMenu fontSize={'2rem'}/> :<IoClose fontSize={'2rem'} />}
           </button>
+          <ul className={`sm:absolute sm:top-20
+          sm:right-0
+           sm:w-full sm:h-full 
+           sm:transition-timing-function: cubic-bezier(0.4, 0, 1, 1)
+           ${isOpen ? 'sm:flex sm:flex-col' : 'hidden'}
+          sm:bg-white sm:items-center 
+          sm:gap-12 lg:hidden max-lg:hidden`}>
+            <li className="leading-normal text-lg ">
+              <Link to="/home">Home</Link>
+            </li>
+            <li className="leading-normal text-lg">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className="leading-normal text-lg">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="leading-normal text-lg">
+              <Link to="/account">Account</Link>
+            </li>
+          </ul>
           <div>
             <Link to="/">
               <img
@@ -22,14 +48,19 @@ function Header() {
             </Link>
           </div>
           <ul className="flex justify-center items-center gap-12 sm:hidden lg:flex max-lg:hidden">
-            <li className="leading-normal text-lg "> <Link to="/home">Home</Link></li>
-            <li className="leading-normal text-lg"><Link to="/contact">Contact</Link></li>
-            <li className="leading-normal text-lg"><Link to="/about">About</Link></li>
-            <li className="leading-normal text-lg"><Link to="/account">Account</Link></li>
+            <li className="leading-normal text-lg ">
+              <Link to="/home">Home</Link>
+            </li>
+            <li className="leading-normal text-lg">
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li className="leading-normal text-lg">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="leading-normal text-lg">
+              <Link to="/account">Account</Link>
+            </li>
           </ul>
-          <div>
-
-          </div>
         </nav>
       </header>
     </div>
